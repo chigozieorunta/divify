5368d31d1b332bc11ff8a9282667fa1d1b776ca7
Chigozie Orunta <chigozieorunta@Chigozies-MacBook-Pro.local>
2022-04-01 10:22:19 +0100

    Added new Gallery extension/module

---
+++ includes/modules/MyGallery/MyGallery.php
@@ -0,0 +1,41 @@
+<?php
+
+class MyGallery extends ET_Builder_Module {
+
+	public $slug       = 'my_gallery';
+	public $vb_support = 'on';
+
+	public function init() {
+		$this->name = esc_html__( 'My Gallery', 'my-gallery' );
+	}
+
+	public function get_fields() {
+		return array(
+			'heading'     => array(
+				'label'           => esc_html__( 'Heading', 'my-gallery' ),
+				'type'            => 'text',
+				'option_category' => 'basic_option',
+				'description'     => esc_html__( 'Input your desired heading here.', 'my-gallery' ),
+				'toggle_slug'     => 'main_content',
+			),
+			'content'     => array(
+				'label'           => esc_html__( 'Content', 'my-gallery' ),
+				'type'            => 'tiny_mce',
+				'option_category' => 'basic_option',
+				'description'     => esc_html__( 'Content entered here will appear below the heading text.', 'my-gallery' ),
+				'toggle_slug'     => 'main_content',
+			),
+		);
+	}
+
+	public function render( $unprocessed_props, $content, $render_slug ) {
+		return sprintf(
+			'<h1 class="my-gallery-heading">%1$s</h1>
+			<div class="my-gallery-content">%2$s</div>',
+			esc_html( $this->props['heading'] ),
+			$this->props['content']
+		);
+	}
+}
+
+new SIMP_SimpleHeader;
