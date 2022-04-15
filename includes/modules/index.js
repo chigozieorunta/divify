ad351be18c7d603e9217b032bb069c84b33d2075
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-04-15 01:49:27 +0100

    Added ComboBox to list of exported JSX modules

--- includes/modules/index.js
+++ includes/modules/index.js
@@ -1,9 +1,11 @@
 import HelloWorld from './HelloWorld/HelloWorld';
 import SimpleHeader from './SimpleHeader/SimpleHeader';
 import MyGallery from './MyGallery/MyGallery';
+import ComboBox from './ComboBox/ComboBox';
 
 export default [
   HelloWorld,
   SimpleHeader,
-  MyGallery
+  MyGallery,
+  ComboBox
 ];
