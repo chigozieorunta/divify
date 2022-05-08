fdb5681d5c35f80fd8416e3f3ec76de752f30adc
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-05-08 10:06:39 +0100

    Retrieve image src from ul and assign to newly loaded DOM image

--- includes/modules/MyGallery/gallery.js
+++ includes/modules/MyGallery/gallery.js
@@ -2,6 +2,7 @@
    $(document).ready(function(){
 	   $('.my-gallery > li > img').click(function () {
 		   $('.my-gallery-overlay').fadeIn('slow');
+		   $('.my-gallery-overlay > img').attr('src', $(this).attr('src'));
 	   });
 
 	   $('.my-gallery-overlay').click(function () {
