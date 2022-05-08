83730ebc9970c8f5564f0f2d66d271f128c53d69
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-05-08 09:39:09 +0100

    Added overlay on page display when clicked

--- includes/modules/MyGallery/gallery.js
+++ includes/modules/MyGallery/gallery.js
@@ -5,8 +5,7 @@
 (function($) {
    $(document).ready(function(){
 	   $('.my-gallery > li > img').click(function () {
-		   console.log('Oh when the saints...');
-		   alert('DOM is ready');
+		   $('.my-gallery-overlay').fadeIn('slow');
 	   });
    });
 })(jQuery);
