cf6a90ef333b3daaaa6f360878257589c8e5fac6
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-05-19 18:11:39 +0100

    Setup new Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+const { src, dest, watch } = require('gulp');
+
+const minify = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
