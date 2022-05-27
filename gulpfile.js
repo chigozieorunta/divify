6bde88a0915a6ecfd0d356b2fb3a223d60462dd0
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-05-27 18:18:05 +0100

    Added gulp compile script

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,12 @@ const { src, dest, watch } = require('gulp');
 const minify = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass')(require('sass'));
+
+const minify = () => {
+	return src('./includes/**/*.scss')
+		.pipe(minify())
+		.pipe(autoprefixer())
+		.pipe(dest('./'))
+}
+
+exports.sass = compile;
