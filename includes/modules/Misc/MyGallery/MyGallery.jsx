dcc79b68afe11e81663350481ddc2004e0fff4ba
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-07-19 12:06:21 +0100

    --date=4 days ago
    Moved JSX file for Gallery module

---
+++ includes/modules/Misc/MyGallery/MyGallery.jsx
@@ -0,0 +1,61 @@
+import React, { Component, Fragment } from 'react';
+	
+import './gallery.css';
+
+class Photo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photo: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch(`http://divify.local/wp-json/wp/v2/media/${this.props.id}`)
+      .then((response) => response.json())
+      .then(singlePhoto => {
+        this.setState({ photo: singlePhoto });
+      });
+  }
+
+  render() {
+    return <img src={this.state.photo.source_url} alt={this.props.alt} />;
+  }
+}
+	
+class MyGallery extends Component {
+	
+  static slug = 'my_gallery';
+
+  state = {
+    photos: []
+  }
+
+  componentDidMount() {
+    fetch('http://divify.local/wp-json/wp/v2/gallery')
+    .then((response) => response.json())
+    .then(allPhotos => {
+        this.setState({ photos: allPhotos });
+    })
+    .catch(error => console.log('Error:', error));
+  }
+  	
+  render() {
+    return (
+		  <Fragment>
+			  <h1 className="my-gallery-heading">{this.props.heading}</h1>
+			  <div className="my-gallery-content">{this.props.content()}</div>
+			  <ul className="my-gallery">
+				  {this.state.photos.map((photo) => (
+					  <li key={photo.id}>
+              <Photo id={photo.featured_media} alt={photo.title.rendered} />
+              <p>{photo.title.rendered}</p>
+					  </li>
+				  ))}
+			  </ul>
+		  </Fragment>
+    );
+  }
+}
+          	
+export default MyGallery;
