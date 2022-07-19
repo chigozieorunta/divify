765946387d0e5b62e7ca5c655a894ec39d962338
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-07-19 11:59:50 +0100

    Changed import file locations

--- includes/modules/index.js
+++ includes/modules/index.js
@@ -1,11 +1,15 @@
-import HelloWorld from './HelloWorld/HelloWorld';
-import SimpleHeader from './SimpleHeader/SimpleHeader';
-import MyGallery from './MyGallery/MyGallery';
-import ComboBox from './ComboBox/ComboBox';
+import HelloWorld from './Misc/HelloWorld/HelloWorld';
+import SimpleHeader from './Misc/SimpleHeader/SimpleHeader';
+import MyGallery from './Misc/MyGallery/MyGallery';
+import ComboBox from './Misc/ComboBox/ComboBox';
+
+import Search from './Realify/Search/Search';
 
 export default [
   HelloWorld,
   SimpleHeader,
   MyGallery,
-  ComboBox
+  ComboBox,
+
+  Search
 ];
