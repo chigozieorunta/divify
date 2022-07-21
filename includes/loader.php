8dd163330426a77b26e85937d1ff3045f221b813
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-07-21 23:12:53 +0100

    Added new Featured file to list of modules to be loaded

--- includes/loader.php
+++ includes/loader.php
@@ -7,10 +7,11 @@ if ( ! class_exists( 'ET_Builder_Element' ) ) {
 //$module_files = glob( __DIR__ . '/modules/**/*.php' );
 
 require_once __DIR__ . '/modules/Realify/Search/Search.php';
+require_once __DIR__ . '/modules/Realify/Featured/Featured.php';
 
 // Load custom Divi Builder modules
-foreach ( (array) $module_files as $module_file ) {
+/*foreach ( (array) $module_files as $module_file ) {
 	if ( $module_file && preg_match( "/\/modules\/\b([^\/]+)\/\\1\.php$/", $module_file ) ) {
 		require_once $module_file;
 	}
-}
+}*/
