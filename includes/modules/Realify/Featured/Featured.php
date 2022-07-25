29ffff5a49da2d3b147e6c1bf9a0bb4f0cd43d9d
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-07-25 13:41:11 +0100

    New changes to Featured module

--- includes/modules/Realify/Featured/Featured.php
+++ includes/modules/Realify/Featured/Featured.php
@@ -164,9 +164,10 @@ class Featured extends ET_Builder_Module {
 			$custom_posts .= sprintf(
 				'<li>
 					<a href="%1$s">
-						<div 
-						<div>
-							<h2>3 Bedroom Semi-Detached Duplex</h2>
+						<div class="realify_featured__img" style="background-image: url(%2$s);">
+						</div>
+						<div class="realify_featured__title">
+							<h2>%3$s</h2>
 							<p>
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
 									<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
@@ -178,32 +179,9 @@ class Featured extends ET_Builder_Module {
 					</a>
 				</li>',
 				$post_url,
-				$post_image
-			);
-			/*$post_image = sprintf( 
-				'<a href="%2$s"><img src="%1$s"></a>',
 				$post_image,
-				$post_url
+				$post_title
 			);
-
-			$post_title = sprintf( 
-				'<a href="%2$s">%1$s</a>',
-				$post_title,
-				$post_url
-			);
-
-			$post_details = sprintf(
-				'<div><h3>%1$s</h3><div>%2$s</div></div>',
-				$post_title,
-				$post_excerpt
-			);
-
-			$custom_posts .= sprintf( 
-				'<div>%1$s%2$s</div>',
-				$post_image,
-				$post_details
-			);*/
-
 		}
 
 		return $custom_posts;
